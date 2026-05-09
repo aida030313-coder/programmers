5aafb69536644eb4aed08926a49f3faddd9b84da
aida030313-coder <aida030313@g.eulji.ac.kr>
2026-05-10 04:11:41 +0900

    0510 업로드

---
+++ 0510_대문자와소문자.py
@@ -0,0 +1,12 @@
+def solution(my_string):
+    answer = ''
+    for i in my_string:
+        if i.isupper():
+            answer += i.lower()
+        else:
+            answer += i.upper()
+    return answer
+
+
+def solution(my_string):
+    return my_string.swapcase()
